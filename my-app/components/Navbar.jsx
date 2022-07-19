de5d3dca5dcacb7b1d17add92aa5c10f3d8b13b0
TroyBohrer8 <81994337+TroyBohrer8@users.noreply.github.com>
2022-07-19 14:35:09 -0400

    fixed padding bug

--- my-app/components/Navbar.jsx
+++ my-app/components/Navbar.jsx
@@ -5,7 +5,7 @@ import React from "react";
 const Navbar = () => {
   return (
     <div className="fixed w-full h-20 shadow-xl z-[100]">
-      <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
+      <div className="flex justify-between items-center w-full h-full px-2 md:px-16">
         <Image
           src="/../public/assets/logo.png"
           alt=""
