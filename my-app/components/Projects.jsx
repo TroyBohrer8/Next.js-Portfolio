0f731bafe62c8e19a29dafbf1ab371c5756d51a6
TroyBohrer8 <81994337+TroyBohrer8@users.noreply.github.com>
2022-08-16 13:21:25 -0400

    completed project section for now, need to update cards to correct projects

--- my-app/components/Projects.jsx
+++ my-app/components/Projects.jsx
@@ -1,7 +1,6 @@
 import React from "react";
-import Image from "next/image";
-import Link from "next/link";
-import randomPasswordImg from "../public/assets/projects/RandomPassword.png"
+import ProjectItem from "../components/ProjectItem";
+import randomPasswordImg from "../public/assets/projects/RandomPassword.png";
 
 const Projects = () => {
   return (
@@ -12,18 +11,30 @@ const Projects = () => {
         </p>
         <h2>What I've Built</h2>
         <div className="grid md:grid-cols-2 gap-8">
-          <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]">
-            <Image className="rounded-xl group-hover:opacity-10" src={randomPasswordImg} alt="/" />
-            <div className="hidden group-hover:block absolute top-[50%] left-[50] translate-x-2">
-              <h3 className="text-2xl text-white tracking-wider text-center">
-                Random Password Generator
-              </h3>
-              <p className="pb-4 pt-2 text-white text-center">Javascript</p>
-              <Link href="/">
-                <p className="text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer">More Info</p>
-              </Link>
-            </div>
-          </div>
+          {/* Project One */}
+          <ProjectItem
+            title="Random Password Generator"
+            backgroundImg={randomPasswordImg}
+            projectUrl="https://troybohrer8.github.io/Random-Password-Generator/"
+          />
+          {/* Project Two */}
+          <ProjectItem
+            title="Random Password Generator"
+            backgroundImg={randomPasswordImg}
+            projectUrl=""
+          />
+          {/* Project Three */}
+          <ProjectItem
+            title="Random Password Generator"
+            backgroundImg={randomPasswordImg}
+            projectUrl=""
+          />
+          {/* Project Four */}
+          <ProjectItem
+            title="Random Password Generator"
+            backgroundImg={randomPasswordImg}
+            projectUrl=""
+          />
         </div>
       </div>
     </div>
